00069ba5ec4e82d405a39c16cd2a04e4bfbbf773
Karan Jadhav <karanjadhav2508@gmail.com>
2017-02-18 20:30:10 -0500

    Base64 and REST api

--- face_recognition/Farecat/app/src/main/java/com/farecat/farecat/MainActivity.java
+++ face_recognition/Farecat/app/src/main/java/com/farecat/farecat/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.media.Image;
+import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,12 +14,36 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Map;
+
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    ImageView photoView;
+    static final String KAIROS_DOMAIN = "https://api.kairos.com";
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private final OkHttpClient client = new OkHttpClient();
+    private final Gson gson = new Gson();
+
+    private ImageView photoView;
+    private String apiService;
+    private Bitmap bitmapPhoto;
+
+    static final String TAG = "karansMessage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void takePhoto(View v) {
+        apiService = ((Button)v).getText().toString().toLowerCase();
         Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(photoIntent, REQUEST_IMAGE_CAPTURE);
     }
@@ -49,10 +75,12 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode == REQUEST_IMAGE_CAPTURE  && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap photo = (Bitmap) extras.get("data");
-            photoView.setImageBitmap(photo);
+            photoView.setImageBitmap(photo);    //remove later. just for reference
+            bitmapPhoto = photo;
 
             //Speculative code
-            String base64String = bitmapToBase64(photo);
+            String base64Photo = bitmapToBase64(photo);
+            new KairosApiTask().execute(base64Photo);
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
@@ -62,5 +90,58 @@ public class MainActivity extends AppCompatActivity {
         bm.compress(Bitmap.CompressFormat.PNG, 100, baos);
         byte[] ba = baos.toByteArray();
         return Base64.encodeToString(ba, Base64.DEFAULT);
+    }
+
+    private class KairosApiTask extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String base64Photo = params[0];
+            JSONObject json = new JSONObject();
+            try {
+                json.putOpt("image", base64Photo);
+                json.putOpt("subject_id", "karan");
+                json.putOpt("gallery_name", "karan");
+            } catch(JSONException j) {
+                Log.i(TAG, "json : "+j.toString());
+            }
+            RequestBody requestBody = RequestBody.create(JSON, json.toString());
+            Request request = new Request.Builder()
+                    .url(KAIROS_DOMAIN+"/"+apiService)
+                    .addHeader("app_id", "76395980")
+                    .addHeader("app_key", "fe00f9411bbadb0269b4f0757050aaf2")
+                    .addHeader("Content-Type", "application/json")
+                    .post(requestBody)
+                    .build();
+
+            Response response = null;
+            try {
+                response = client.newCall(request).execute();
+            }
+            catch(IOException i) {
+                Log.i(TAG, "response : "+i.toString());
+            }
+            if(response != null && response.isSuccessful()) {
+                Log.i(TAG, "Success!");
+                try {
+                    //Log.i(TAG, "response : "+response.body().string());
+                    try {
+                        JSONObject jo = new JSONObject(response.body().string());
+                        Log.i(TAG, "parsedStatus : "+jo.getJSONArray("images")
+                                .getJSONObject(0)
+                                .getJSONObject("transaction")
+                                .getString("status")
+                        );
+                    } catch(JSONException j) {
+                        j.printStackTrace();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else {
+                Log.i(TAG, "Hag diya");
+            }
+            return response.toString();
+        }
     }
 }
