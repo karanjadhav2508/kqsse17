9c2628ab70730c4a7bf1cea6465e26b721be5228
Samantha Scoggins <smscoggi@ncsu.edu>
2017-02-27 20:29:21 -0500

    Add files via upload

---
+++ Engagement/EngageTendance - connected database, login functionality finish (no hash)/src/engagetendance/panel.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package engagetendance;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author Sam
+ */
+public class panel {
+    
+    
+    String user;
+    String name;
+    int role;
+    
+    panel(String user, String name, int role){
+        
+            this.user = user;
+            this.role = role;
+            this.name = name;
+        
+        
+        
+    }
+    
+    
+   
+    
+    
+    
+}
